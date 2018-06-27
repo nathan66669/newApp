793b7e523419e0fb673c49232d88c47dee180454
nathan <37780747+nathan66669@users.noreply.github.com>
2018-06-27 16:01:39 -0400

    edited date

--- app/helpers/format-date.js
+++ app/helpers/format-date.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export function formatDate(params){
-	return moment(params[0]).format('YYYY-MM-DD');
+	return moment(params[0]).format('DD MMM YY');
 }
 
 export default Ember.Helper.helper(formatDate);
