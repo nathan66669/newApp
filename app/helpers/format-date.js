c9037d5512992616e6f7e93c6ebedc4114cd1070
nathan <37780747+nathan66669@users.noreply.github.com>
2018-06-28 09:14:52 -0400

    switched to new import style

--- app/helpers/format-date.js
+++ app/helpers/format-date.js
@@ -1,7 +1,6 @@
-import Ember from 'ember';
-
+//import Ember from '@ember';
+import Helper from '@ember/component/helper'
 export function formatDate(params){
 	return moment(params[0]).format('DD MMM YY');
 }
-
-export default Ember.Helper.helper(formatDate);
+export default Helper.helper(formatDate);
--- app/routes/clans.js
+++ app/routes/clans.js
@@ -1,6 +1,6 @@
-import Ember from 'ember';
-
-export default Ember.Route.extend({
+//import Ember from 'ember';
+import Route from '@ember/routing/route'
+export default Route.extend({
 	model: function(){
 		return this.store.findAll('clan');
 	}
