46d09e2ad108357c18a9f46921c153358b836cfb
nathan <37780747+nathan66669@users.noreply.github.com>
2018-07-20 14:25:23 -0400

    updated

--- app/models/cwar.js
+++ app/models/cwar.js
@@ -2,5 +2,5 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
 	//cwar: DS.hasMany('cwar', {async:true,inverse:null}),
-	oname: DS.belongsTo('cwar')
+	oname: DS.attr('string')
 });
